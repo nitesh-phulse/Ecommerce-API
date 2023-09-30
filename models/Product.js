cb39f444e88916200afeef4b5e73a00dc9fbd055
Nethish P <98277459+nitesh-phulse@users.noreply.github.com>
2023-09-30 11:36:32 +0530

    Product.js

--- models/Product.js
+++ models/Product.js
@@ -1,9 +1,15 @@
 // models/Product.js
+
+// Import the mongoose library
 const mongoose = require('mongoose');
 
+// Define a schema for the 'Product' model
 const productSchema = new mongoose.Schema({
+  // Define a field 'name' of type String which is required
   name: { type:  String, required: true },
+  // Define a field 'quantity' of type Number which is required
   quantity: { type: Number, required: true },
 });
 
+// Create and export the 'Product' model using the defined schema
 module.exports = mongoose.model('Product', productSchema);
